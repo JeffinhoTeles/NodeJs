05b0c3995149c6d6ae8beaa0bac4363177d2504f
jeffinhoo <jefferson.teles@fmu.br>
2025-03-18 12:39:02 -0300

    Entrega parcial 1 do projeto final

--- Products/src/app.js
+++ Products/src/app.js
@@ -1,30 +1,12 @@
 const express = require("express");
-const ProductManager = require("./ProductManager");
+const productsRouter = require("./routes/productsRouter");
+const cartsRouter = require("./routes/cartsRouter");
 
 const app = express();
-const port = 8080;
-const productManager = new ProductManager("products.json");
+app.use(express.json());
 
-// Endpoint para listar produtos (com limite opcional)
-app.get("/products", async (req, res) => {
-  const limit = parseInt(req.query.limit);
-  const products = await productManager.getProducts(limit);
-  res.json(products);
-});
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 
-// Endpoint para buscar um produto pelo ID
-app.get("/products/:pid", async (req, res) => {
-  const productId = parseInt(req.params.pid);
-  const product = await productManager.getProductById(productId);
-
-  if (!product) {
-    return res.status(404).json({ error: "Produto não encontrado" });
-  }
-
-  res.json(product);
-});
-
-// Inicia o servidor
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
